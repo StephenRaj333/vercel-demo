947c86237e2625012b369ed4c5b5c39e71eaea50
stephen_M2P <stephen+M2P@m2pfintech.com>
2024-05-28 10:48:02 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const app = express();
 
 // Use the PORT environment variable, or default to 3000 if not set
 const PORT = process.env.PORT || 3000;
+app.use(express.json()); 
+
+let info = [];  
 
 // Define a simple route
-app.get('/home', (req, res) => {
-    res.send({
-        title: "Stepheh",
-        desc: "description"
-    }); 
+app.get('/get', (req, res) => {
+    res.send(info);
 });
+
+app.post('/post',(req,res) => { 
+    const newData = req.body;  
+    info.push(newData); 
+    res.send("Posted Data Successfully"); 
+}); 
 
 // Start the server
 app.listen(PORT, () => {
